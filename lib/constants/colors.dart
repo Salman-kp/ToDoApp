e82a58f95f31f0fcd9ff05d46a9cac146621d39f
Salman-kp <salmankp7592@gmail.com>
2025-03-08 03:20:54 -0500

    First commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color.fromARGB(255, 238, 5, 5);
+const Color tdBlue = Color.fromARGB(255, 14, 1, 168);
+
+
+const Color tdBlack = Color.fromARGB(216, 0, 0, 0);
+const Color tdGrey = Color.fromARGB(255, 114, 70, 12);
+
+
+const Color tdBGColor = Color.fromARGB(255, 226, 216, 216);
